7bded69ee4db16b4d352e0463889a7de0f990258
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-01-28 20:19:14 -0300

    Arithmetic Operations

---
+++ ARITHMETIC OPERATIONS/ArithmeticOperations.js
@@ -0,0 +1,10 @@
+let number1 = 2;
+let number2 = 4;
+
+console.log('Sum: 2 + 4 = ' + (number1 + number2));
+
+console.log('Subtraction: 2 - 4 = ' + (number1 - number2));
+
+console.log('Division: 2 / 4 = ' + (number1 / number2));
+
+console.log('Multiplication: 2 * 4 = ' + (number1 * number2));
